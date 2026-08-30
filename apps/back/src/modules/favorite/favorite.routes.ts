5fd8373488c4583ccf63e54d34c035a3098152f4
Benjamin CHARLES <benjamin.charles@nexteneo.com>
2026-08-30 13:36:24 +0200

    fix ci

--- apps/back/src/modules/favorite/favorite.routes.ts
+++ apps/back/src/modules/favorite/favorite.routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 export function favoriteRoutes(fastify: FastifyInstance){
     fastify.post('/favorites', async (request: FastifyRequest, reply: FastifyReply) => {
-        console.log(request.body)
+        // console.log(request.body)
     })
 }
